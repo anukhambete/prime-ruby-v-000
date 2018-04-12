fca1a678ce0b3bfa6f07bfd553285bda53235b7a
Anuradha Khambete <anukhambete@gmail.com>
2018-04-12 21:49:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 # Add  code here!
 def prime?(num)
-if num <= 0
-  false
-else
-  foctors = []
-  arr_num = (1..num).to_a
-  factors << arr_num.each do |n|
-    if num % n == 0
-      n
+  if num <= 0
+    false
+  else
+    factors = []
+    arr_num = (1..num).to_a
+    factors << arr_num.each do |n|
+      if num % n == 0
+        n
+      end
     end
   end
-end
 end
